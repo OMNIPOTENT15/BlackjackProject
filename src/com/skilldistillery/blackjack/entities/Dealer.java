cb4c0fdd508d1d23ac3d7ac5982c0c6b19f35141
Blake Vieyra <blake.vieyra@gmail.com>
2023-12-08 17:14:53 -0800

    added more logic, fixed bugs, needs refactoring

--- src/com/skilldistillery/blackjack/entities/Dealer.java
+++ src/com/skilldistillery/blackjack/entities/Dealer.java
@@ -11,109 +11,107 @@ public class Dealer extends Player {
 
 	public void startGame() {
 		System.out.println("Welcome to Blackjack!");
-		boolean playAgain = true;
 
-		Card playerFirstCard = null;
-		Card playerSecondCard = null;
-		Card dealersFirstCard = null;
-		Card dealersSecondCard = null;
-		int playerTotal = 0;
-		while (playAgain) {
-			deck.shuffleDeck();
-			deck.shuffleDeck();
+		deck.shuffleDeck();
+		deck.shuffleDeck();
+		System.out.println();
+
+		Card playerFirstCard = dealingPlayersCard(deck.removeCard());
+		System.out.print("Your first card: " + playerFirstCard.toString());
+		System.out.println(" Value:  " + playerFirstCard.getValue());
+
+		Card playerSecondCard = dealingPlayersCard(deck.removeCard());
+		System.out.print("Your second card:  " + playerSecondCard.toString());
+		System.out.println(" Value:  " + playerSecondCard.getValue());
+
+		int playerTotal = playerFirstCard.getValue() + playerSecondCard.getValue();
+		System.out.println("Your total card value: " + playerTotal);
+		System.out.println();
+
+		Card dealersFirstCard = dealingDealersCard(deck.removeCard());
+		Card dealersSecondCard = dealingDealersCard(deck.removeCard());
+		System.out.println(
+				"Dealers second card: " + dealersSecondCard.toString() + " Value: " + dealersSecondCard.getValue());
+
+		int dealerTotal = dealersFirstCard.getValue() + dealersSecondCard.getValue();
+
+		if (playerTotal == 21) {
 			System.out.println();
-
-			playerFirstCard = dealingPlayersCard(deck.removeCard());
-			System.out.print("You drew a " + playerFirstCard.toString());
-			System.out.println(" Value is " + playerFirstCard.getValue());
-			playerSecondCard = dealingPlayersCard(deck.removeCard());
-			System.out.print("You drew a " + playerSecondCard.toString());
-			System.out.println(" Value is " + playerSecondCard.getValue());
-			playerTotal = playerFirstCard.getValue() + playerSecondCard.getValue();
-			System.out.println("Your total card value " + playerTotal);
+			System.out.println("Blackjack! You win!");
+		} else if (dealerTotal == 21) {
 			System.out.println();
+			System.out.println(
+					"Dealers second card: " + dealersSecondCard.toString() + " Value: " + dealersSecondCard.getValue());
+			System.out.println("Dealer total card value: " + dealerTotal);
+			System.out.println("Blackjack! Dealer wins!");
+		}
+		while (playerTotal < 21) {
+			System.out.println();
+			System.out.println();
+			System.out.println("Would you like to HIT or STAND? Type (1) for HIT, type (2) for STAND.");
+			int choice = sc.nextInt();
 
-			dealersFirstCard = dealingDealersCard(deck.removeCard());
-			dealersSecondCard = dealingDealersCard(deck.removeCard());
-			System.out.println("Dealer drew a " + dealersFirstCard.toString());
+			if (choice == 1) {
+				Card newCard = hit();
+				dealingPlayersCard(newCard);
+				System.out.print("Your next card: " + newCard.toString());
+				System.out.println(" Value: " + newCard.getValue());
+				playerTotal += newCard.getValue();
+				System.out.println("Your total card value: " + playerTotal);
+				if (playerTotal > 21) {
+					System.out.println("You Busted. Dealer wins!");
+					break;
+				}
+				if (playerTotal == 21) {
+					System.out.println();
+					System.out.println("Blackjack! You win!");
+					break;
 
-			int dealerTotal = dealersFirstCard.getValue() + dealersSecondCard.getValue();
+				}
+			} else if (choice == 2) {
+				System.out.println("Dealers second card: " + dealersSecondCard.toString() + "Value: "
+						+ dealersSecondCard.getValue());
+				while (dealerTotal < 21) {
+					if (dealerTotal >= 17 && dealerTotal < 21) {
+						System.out.println("Dealers Total Card Value: " + dealerTotal);
+						if (dealerTotal < playerTotal) {
+							System.out.println("Dealers total card value: " + dealerTotal);
+							System.out.println();
+							System.out.println("Your total card value: " + playerTotal);
+							System.out.println("You win!");
+							break;
 
-			if (playerTotal == 21) {
-				System.out.println();
-				System.out.println("Blackjack! You win!");
-				playAgain = wouldYouLikeToPlayAgain();
-			} else if (dealerTotal == 21) {
-				System.out.println();
-				System.out.println("Blackjack! Dealer wins!");
-				playAgain = wouldYouLikeToPlayAgain();
-			}
-
-			while (playerTotal < 21) {
-				System.out.println();
-				System.out.println();
-				System.out.println("Would you like to hit or stand? Type (1) for HIT, type (2) for STAND.");
-				int choice = sc.nextInt();
-
-				if (choice == 1) {
-					Card newCard = hit();
-					dealingPlayersCard(newCard);
-					System.out.println("You drew a " + newCard.toString());
-					System.out.println("Card Value is " + newCard.getValue());
-					playerTotal += newCard.getValue();
-					System.out.println("Your total card value is : " + playerTotal);
-					if (playerTotal > 21) {
-						System.out.println("You Busted. Dealer wins!");
-						playAgain = wouldYouLikeToPlayAgain();
+						} else {
+							System.out.println("Dealer total card value: " + dealerTotal);
+							System.out.println();
+							System.out.println("Your total card value: " + playerTotal);
+							System.out.println("Dealer wins!");
+							break;
+						}
 					}
-					if (playerTotal == 21) {
-						System.out.println();
-						System.out.println("Blackjack! You win!");
-						playAgain = wouldYouLikeToPlayAgain();
-					}
-				} else if (choice == 2) {
-
 					if (dealerTotal < 17) {
 						Card newCard = hit();
 						dealingDealersCard(newCard);
-						System.out.println("Dealer drew a " + dealersSecondCard.toString());
-						System.out.println("Dealer drew a " + newCard.toString());
+						System.out
+								.println("Dealers next card:  " + newCard.toString() + "Value: " + newCard.getValue());
 						dealerTotal += newCard.getValue();
-						System.out.println("Dealer total card value is " + dealerTotal);
+						System.out.println("Dealer total card value: " + dealerTotal);
 						if (dealerTotal > 21) {
 							System.out.println("Dealer Busted. You win!");
-							playAgain = wouldYouLikeToPlayAgain();
+							break;
+
 						}
 						if (dealerTotal == 21) {
 							System.out.println("Blackjack! Dealer wins!");
-							playAgain = wouldYouLikeToPlayAgain();
+							break;
 						}
-						if (dealerTotal >= 17 && dealerTotal < 21) {
-							System.out.println("Dealer drew a " + dealersSecondCard.toString());
-							System.out.println("Dealer Total Card is " + dealerTotal);
-							if (dealerTotal < playerTotal) {
-								System.out.println("Dealer total card value is " + dealerTotal);
-								System.out.println();
-								System.out.println("Your total card value is " + playerTotal);
-								System.out.println("You win!");
-								playAgain = wouldYouLikeToPlayAgain();
-							} else {
-								System.out.println("Dealer total card value is " + dealerTotal);
-								System.out.println();
-								System.out.println("Your total card value is " + playerTotal);
-								System.out.println("Dealer wins!");
-								playAgain = wouldYouLikeToPlayAgain();
-							}
-						}
-
 					}
 
-				} else {
-					System.out.println("Please enter a valid input.");
 				}
+			} else {
+				System.out.println("Please enter a valid input.");
 			}
 		}
-
 	}
 
 	public Card dealingPlayersCard(Card Card) {
@@ -131,18 +129,19 @@ public class Dealer extends Player {
 		return newCard;
 	}
 
-	public boolean wouldYouLikeToPlayAgain() {
-		System.out.println();
-		System.out.println("Would you like to play again? (1) for yes, and (2) for no.");
-		int choice = sc.nextInt();
-		sc.nextLine();
-		if (choice == 1) {
-			return true;
-		} else if (choice == 2) {
-			System.out.println("Goodbye!");
-		} else {
-			System.out.println("Please choose a valid input.");
-		}
-		return true;
-	}
+//	public boolean wouldYouLikeToPlayAgain() {
+//		System.out.println();
+//		System.out.println("Would you like to play again? (1) for yes, and (2) for no.");
+//		int choice = sc.nextInt();
+//		sc.nextLine();
+//		if (choice == 1) {
+//			return true;
+//		} else if (choice == 2) {
+//			System.out.println("Goodbye!");
+//			return false;
+//		} else {
+//			System.out.println("Please choose a valid input.");
+//		}
+//		return false;
+//	}
 }
